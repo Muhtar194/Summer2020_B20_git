f3d4aa138d62ebefd1ed489104ddec2d5dc218d2
muhatr194 <muhtartu194@gmail.com>
2020-08-05 19:55:56 -0500

    add GitFun.java

--- src/utility/ReverseAString.java
+++ src/utility/ReverseAString.java
@@ -5,7 +5,7 @@ public class ReverseAString {
     // soubsfboabf
     public static void main(String[] args) {
 
-        System.out.println("reverse ");
+        System.out.println("reverse git ");
 
 
 
